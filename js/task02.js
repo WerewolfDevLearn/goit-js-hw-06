ae0265ebdd4d8e8adb8a6d40bba64c247b2a2948
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-01-22 00:19:40 +0200

    fix task_01

--- js/task02.js
+++ js/task02.js
@@ -1,9 +1,9 @@
 const list = document.querySelector('ul#categories');
 
 const numberCategories = list.childElementCount;
-console.log('Number of categories:', numberCategories);
 
 const listChilds = document.querySelectorAll('li.item');
+
 listChilds.forEach(child => {
   console.log('Category:', child.querySelector('h2').textContent);
   console.log('Elements:', child.querySelectorAll('li').length);
